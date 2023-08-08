efbb66673232f792d38b86075948c9f5ccbcf73f
benbenbenbenbenbenbenbenbenben <ben.kersnovske@hotmail.com>
2023-08-08 20:58:52 +0100

    Add functionality to update multiple networks at once. Only Per Connection added for now.

--- app.py
+++ app.py
@@ -277,9 +277,9 @@ def export_to_csv(data, filename):
 #----------------------------------------------------------------------------#
 
 # Debug:
-if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+# if __name__ == "__main__":
+#     app.run(debug=True, port=5001)
 
-# if __name__ == '__main__':
-#     app.run()
+if __name__ == '__main__':
+    app.run()
 
